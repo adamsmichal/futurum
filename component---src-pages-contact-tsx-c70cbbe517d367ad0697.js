62ea43a826d5b4094195fbcca8fe5364aac0f3da
adamsmichal <adamsmichal@wp.pl>
2020-07-01 01:03:08 +0200

    build

---
+++ component---src-pages-contact-tsx-c70cbbe517d367ad0697.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{"0Aie":function(e,a,t){"use strict";t.r(a);var n=t("q1tI"),l=t.n(n),r=t("vOnD"),c=t("iOje"),u=t("JLhn"),i=t("PYgs"),m=t("r1+Y"),o=t("xITp");a.default=function(){return l.a.createElement(l.a.Fragment,null,l.a.createElement(r.a,{theme:c.a},l.a.createElement(u.a,null),l.a.createElement(m.a,{title:"Kontakt"}),l.a.createElement(o.a,null),l.a.createElement(i.a,null)))}}}]);
+//# sourceMappingURL=component---src-pages-contact-tsx-c70cbbe517d367ad0697.js.map
--- gatsby-browser.js
+++
@@ -1,2 +0,0 @@
-import "./src/assets/styles/normalize.css";
-import "./src/assets/styles/toolbox.css";
